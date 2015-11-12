099f0b4a9aa2e972a5e4cd1509d1c16c44a6ca59
kevinweber <k-iwi@web.de>
2015-11-12 18:02:26 -0500

    Added splash screen

--- app/src/main/java/com/meshmix/meshmix/MainActivity.java
+++ app/src/main/java/com/meshmix/meshmix/MainActivity.java
@@ -3,6 +3,7 @@ package com.meshmix.meshmix;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,6 +19,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -56,6 +58,7 @@ public class MainActivity extends Activity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         setupPlayer();
