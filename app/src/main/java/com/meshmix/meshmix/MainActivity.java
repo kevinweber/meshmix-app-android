da26e545f8633e1011b7af7a4cf356a52809b7c1
kevinweber <k-iwi@web.de>
2015-11-13 17:28:26 -0500

    Added menu with two dummy links

--- app/src/main/java/com/meshmix/meshmix/MainActivity.java
+++ app/src/main/java/com/meshmix/meshmix/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -162,6 +163,7 @@ public class MainActivity extends Activity implements
         }
     }
 
+    // @TODO: Handle button clicks (play music etc.) when user has no internet connection so that the app doesn't crash
     public void setupPlayer() {
 
         /////////////////////////////////
@@ -202,6 +204,7 @@ public class MainActivity extends Activity implements
             public void onClick(View v) {
                 prev();
             }
+
             public void prev() {
                 mPlayer.skipToPrevious();
             }
@@ -265,6 +268,7 @@ public class MainActivity extends Activity implements
         }
     }
 
+
     @Override
     protected void onDestroy() {
         // VERY IMPORTANT! This must always be called or else you will leak resources
@@ -272,6 +276,31 @@ public class MainActivity extends Activity implements
         myTTS.stop();
         myTTS.shutdown();
         super.onDestroy();
+    }
+
+
+    // SETUP MENU
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Log.d("MainActivity", "Clicked on 'action_settings");
+                return true;
+            case R.id.feedback_link:
+                Log.d("MainActivity", "Clicked on 'feedback_link");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
